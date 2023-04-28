b0e626d93b9308358ecc308f022ff673993a3a0e
=Andrei <balasca.andrei@gmail.com>
2023-04-28 21:46:36 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryItem = creategalleryItems(galleryItems);
+
+function creategalleryItems(items) {
+    return items.map(({ preview, original, description }) => {
+        return `<div class = "galllery__item">
+        <a href = "${original}">
+        <img src = "${preview}" data-source = "${original}" alt = "${description}"/>
+        </a>
+        </div>`;
+        });
+}
+
+gallery.append(galleryItem);
+
+
+console.log(galleryItem);
