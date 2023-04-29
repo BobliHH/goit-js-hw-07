b9bd655a647177970a5df41235e466fe3d874627
=Andrei <balasca.andrei@gmail.com>
2023-04-29 22:06:04 +0300

    created grid

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,20 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector('.gallery');
+const gallery = document.querySelector(".gallery");
 const galleryItem = creategalleryItems(galleryItems);
 
 function creategalleryItems(items) {
-    return items.map(({ preview, original, description }) => {
+    return items
+      .map(({ preview, original, description }) => {
         return `<div class = "galllery__item">
-        <a href = "${original}">
-        <img src = "${preview}" data-source = "${original}" alt = "${description}"/>
+        <a class = "gallery__link" href = "${original}">
+        <img class = "gallery__image" src = "${preview}" 
+        data-source = "${original}" alt = "${description}"/>
         </a>
         </div>`;
-        });
+      })
+      .join(" ");
 }
 
-gallery.append(galleryItem);
+gallery.insertAdjacentHTML('beforeend', galleryItem);
 
 
 console.log(galleryItem);
